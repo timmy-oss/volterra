77323b6a29bab8b52673408675351de36c56eaf8
Timileyin Pelumi <dev.timileyinpelumi@gmail.com>
2021-11-03 20:40:04 +0100

    Changes to be committed: 	modified:   .env 	modified:   app.js 	new file:   bot/index.js 	modified:   package.json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const {admitNewReferral} = require('./airdrop/controllers');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const {bot} = require('./bot/index');
 
 app.set('view engine', 'ejs');
 app.set('view options', {delimiter: '?'});
@@ -14,11 +15,17 @@ app.set('view options', {delimiter: '?'});
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.render('index');
+});
+
+app.post(`/${process.env.TG_TOKEN}`, (req, res) => {
+	bot.processUpdate(req.body);
+	res.status(200).end();
 });
 
 app.get('/volterra-airdrop/', (req, res) => {
---
+++ bot/index.js
@@ -0,0 +1,53 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const token = process.env.TG_TOKEN;
+let bot;
+
+if (process.env.NODE_ENV === 'production') {
+	bot = new TelegramBot(token);
+	bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+	bot = new TelegramBot(token, {polling: true});
+}
+
+bot.onText(/\/start/, (msg) => {
+	bot.sendMessage(
+		msg.chat.id,
+		`
+
+    Hello! ${msg.chat.username}, Welcome To Volterra Ecosystem
+
+🔐 Please Complete All The Required Tasks To Get Airdrop Rewards.
+
+🏄 Join Volterra Ecosystem Telegram Channel (https://t.me/Volterratoken) and Group
+ (https://t.me/volterranews)🏄 Follow Volterra Ecosystem Official Twitter (https://twitter.com/Volterra_Eco) & Retweet Pinned Tweet
+🏄 Follow Volterra President Twitter
+ (https://twitter.com/michaeljimoh0)
+🏄 Follow On Linkedin (https://www.linkedin.com/company/volterra-ecosystem)
+🏄 Follow On Reddit  (https://www.reddit.com/r/Volterra_Ecosystem?utm_medium=android_app&utm_source=share)
+🏄 Submit your Binance Smart Chain BEP20 Wallet Address
+
+📘By Participating You Are Agreeing To Volterra Airdrop Program Terms and Conditions.
+
+Click "🚀 Join Airdrop" to proceed
+
+
+
+
+
+    `,
+		{
+			reply_markup: {
+				keyboard: [['Join  Airdrop']],
+				resize_keyboard: true,
+				one_time_keyboard: false,
+				force_reply: true,
+			},
+		}
+	);
+});
+
+module.exports = {
+	bot,
+};
